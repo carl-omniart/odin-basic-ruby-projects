4c213217505144ee83e8b0a688f1447fd33cd455
Carl Berndash Omniart <[50526700+carl-omniart@users.noreply.github.com]>
2024-08-28 15:05:23 -0700

    Add caesar_cipher code

---
+++ lib/caesar_cipher.rb
@@ -0,0 +1,17 @@
+def caesar_cipher string, offset
+  string.each_char.reduce("") do |encoded, char|
+    ord = char.ord
+    ord = shift ord, offset, ("a".ord.."z".ord)
+    ord = shift ord, offset, ("A".ord.."Z".ord) 
+    encoded << ord.chr
+  end
+end
+
+def shift value, offset, range
+  if range.cover? value
+    value += offset
+    value += range.size while value < range.min
+    value -= range.size while value > range.max
+  end
+  value
+end
