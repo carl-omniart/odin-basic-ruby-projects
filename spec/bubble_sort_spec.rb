23aa107eac42568a7bd089425450e66a48afd735
Carl Berndash Omniart <[50526700+carl-omniart@users.noreply.github.com]>
2024-08-28 19:56:51 -0700

    Add bubble_sort specs

---
+++ spec/bubble_sort_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper.rb'
+require_relative '../lib/bubble_sort.rb'
+
+RSpec.describe 'Bubble Sort Project' do
+  it 'sorts!' do
+    expect(bubble_sort([4, 3, 78, 2, 0, 2])).to eq([0, 2, 2, 3, 4, 78])
+    expect(bubble_sort([87, 26, 74, 33, 59, 52]).to eq([26, 33, 52, 59, 74, 87])
+    expect(bubble_sort([11, 13, 11, 13, 6, 13])).to eq([6, 11, 11, 13, 13, 13])
+  end
+end
